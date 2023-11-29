672b797fead219186b53d2c7278228bb076b63e4
gustika17 <112964518+gustika17@users.noreply.github.com>
2023-11-29 22:26:28 +0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,2 +1,23 @@
 public class Admin {
+    private String username;
+    private String password;
+
+    public static void addkekolaanggota() {
+        System.out.println("Notifikasi kekola Anggota Berhasil");
+    }
+
+    public static void kekolaBuku(){
+    System.out.println("Kelola Buku Berhasil");
+}
+public static void generateLaporan(){
+    System.out.println("Laporan Di terima");
+}
+public String getUsername(){
+    return username;
+    }
+    public void setUsername(String username){
+    this.username = username;
+
+    }
+
 }
