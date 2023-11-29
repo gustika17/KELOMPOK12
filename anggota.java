9279c9c0b1f92e3ac9727f5457434bbebe979e07
gustika17 <112964518+gustika17@users.noreply.github.com>
2023-11-29 22:27:37 +0700

    Update anggota.java

--- anggota.java
+++ anggota.java
@@ -1,20 +1,48 @@
-
-import java.util.ArrayList;
 import java.util.List;
-public class anggota {
+
+public class Anggota  {
     private String nama;
     private String  nomorAnggota;
     private String alamat;
-    public ArrayList<anggota>riwayatPeminjaman;
+    public List<String> riwayatPeminjaman;
 
-    public String getDetails(){
+    public Anggota(String nama, String nomorAnggota,String alamat) {
         this.nama = nama;
         this.nomorAnggota = nomorAnggota;
         this.alamat = alamat;
-        return null;
 
     }
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNomorAnggota() {
+        return nomorAnggota;
+    }
+
+    public void setNomorAnggota(String nomorAnggota) {
+        this.nomorAnggota = nomorAnggota;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    public String getDetails(){
+        return "Anggota : " + nama + ", Nomor Anggota : " + nomorAnggota + ", Alamat " + alamat;
+    }
+    public boolean Login(String inputNomorAnggota, String password){
+        return nomorAnggota.equals(inputNomorAnggota) && password.equals("");
+    }
+
+    public void addkekolabuku() {
+        System.out.println("Notifikasi diterima");
+    }
 }
-
-
-
