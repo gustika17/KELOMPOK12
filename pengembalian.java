634b0514b756cfbb948b0155ea10bade24c9d617
gustika17 <112964518+gustika17@users.noreply.github.com>
2023-11-29 22:28:57 +0700

    Update pengembalian.java

--- pengembalian.java
+++ pengembalian.java
@@ -1,2 +1,37 @@
-public class pengembalian {
-}
+import java.util.Date;
+public class pengembalian  {
+     private Buku buku;
+     private Anggota anggota;
+     private Date tanggalPengembalian;
+
+
+     public pengembalian(Buku buku, Anggota anggota, Date tanggalpengembalian) {
+         this.buku = buku;
+         this.anggota = anggota;
+         this.tanggalPengembalian = new Date();
+     }
+         public Buku getBuku() {
+             return buku;
+         }
+
+         public void setBuku(Buku buku) {
+             this.buku = buku;
+         }
+
+         public Anggota getAnggota() {
+             return anggota;
+         }
+
+         public void setAnggota(Anggota anggota) {
+             this.anggota = anggota;
+         }
+
+         public Date getTanggalPengembalian() {
+             return tanggalPengembalian;
+         }
+
+         public void setTanggalPengembalian(Date tanggalPengembalian) {
+             this.tanggalPengembalian = tanggalPengembalian;
+         }
+
+     }
