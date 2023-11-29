489e9ce7f70cbf469f9dc366419eda6ab0f86db1
gustika17 <112964518+gustika17@users.noreply.github.com>
2023-11-29 22:26:52 +0700

    Update Notifikasi.java

--- Notifikasi.java
+++ Notifikasi.java
@@ -1,2 +1,23 @@
 public class Notifikasi {
+    private String pesan;
+
+    private Anggota penerima;
+
+    public Notifikasi(String pesan, Anggota penerima){
+        this.pesan = pesan;
+        this.penerima = penerima;
+    }
+    public String getPesan(){
+        return pesan;
+    }
+    public void setPesan(String pesan){
+        this.pesan = pesan;
+    }
+    public Anggota getPenerima(){
+        return penerima;
+    }
+    public void setPenerima(Anggota penerima){
+        this.penerima = penerima;
+
+    }
 }
