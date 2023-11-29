e36ab0403d50d28878ca192bbe71d84ee00f20bf
gustika17 <112964518+gustika17@users.noreply.github.com>
2023-11-29 22:25:41 +0700

    Update Buku.java

--- Buku.java
+++ Buku.java
@@ -4,13 +4,52 @@ public class Buku {
     private String nomorISBN ;
     private boolean statusKesediaan;
 
-    public String getDetails(){
-        this.judul = judul;
-        this.pengarang= pengarang;
-        this.nomorISBN = nomorISBN;
-        this.statusKesediaan = statusKesediaan;
-        return null;
+   public Buku(String judul,String pengarang, String nomorISBN){
+       this.judul = judul;
+       this.pengarang = pengarang;
+       this.nomorISBN = nomorISBN;
+       this.statusKesediaan = true;
+    }
+    public String getJudul() {
+        
+       return judul;
     }
 
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
 
+    public String getPengarang() {
+
+       return pengarang;
+    }
+
+    public void setPengarang(String pengarang) {
+
+       this.pengarang = pengarang;
+    }
+
+    public String getNomorISBN() {
+
+       return nomorISBN;
+    }
+
+    public void setNomorISBN(String nomorISBN) {
+
+       this.nomorISBN = nomorISBN;
+    }
+
+    public boolean isStatusKesediaan() {
+
+       return statusKesediaan;
+    }
+
+    public void setStatusKesediaan(boolean statusKesediaan) {
+        this.statusKesediaan = statusKesediaan;
+    }
+    public String getDetails(){
+       return "Buku : " + judul + "nama pengarang " + pengarang + "ISBN" + nomorISBN;
+
+
+    }
 }
